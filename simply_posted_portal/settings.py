1e89d61265c4fad9f17965a71412a203d8053b26
Tyler Baker <tyler.baker@linaro.org>
2017-01-04 12:06:50 -0800

    simply-posted-portal: add gmail auth
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

--- simply_posted_portal/settings.py
+++ simply_posted_portal/settings.py
@@ -178,7 +178,12 @@ FIXTURE_DIRS = [
     os.path.join(PROJECT_ROOT, "fixtures"),
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER =  os.environ.get("EMAIL_ACCOUNT")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
 
 ACCOUNT_OPEN_SIGNUP = True
 ACCOUNT_EMAIL_UNIQUE = True
